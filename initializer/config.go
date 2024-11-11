c0adcb1c622d293128273c6e474a061c001b8bdf
trace-panic <patrickscript@proton.me>
2024-11-11 21:24:01 +0300

    Create system files on /user/linux/*

--- initializer/config.go
+++ initializer/config.go
@@ -4,13 +4,14 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"os/user"
 	"strings"
+	"time"
 )
 
 type Config struct {
-	Username      string
-	Hostname      string
-	RootDirectory string
+	Username string
+	Hostname string
 }
 
 var config Config
@@ -32,18 +33,35 @@ func CreateConfig() error {
 	fmt.Print("Enter computer hostname (e.g., dell): ")
 	hostname := ReadInput()
 
-	fmt.Print("Enter root directory name (e.g., filesystem): ")
-	rootDirectory := ReadInput()
-
 	config = Config{
-		Username:      username,
-		Hostname:      hostname,
-		RootDirectory: rootDirectory,
+		Username: username,
+		Hostname: hostname,
 	}
 
-	if err := os.Mkdir(rootDirectory, 0750); err != nil {
+	usr, err := user.Current()
+	if err != nil {
 		return err
 	}
+
+	baseDir := usr.HomeDir + "/linux"
+	homeDir := baseDir + "/home/" + username
+	tempDir := baseDir + "/temp"
+	varsDir := baseDir + "/vars"
+	directories := []string{homeDir, tempDir, varsDir}
+
+	for _, dir := range directories {
+		if _, err := os.Stat(dir); os.IsNotExist(err) {
+			if err := os.MkdirAll(dir, 0750); err != nil {
+				return err
+			}
+			fmt.Printf("Directory created: %s\n", dir)
+		} else {
+			fmt.Printf("Directory already exists: %s\n", dir)
+		}
+	}
+
+	fmt.Println("Operation complete. Continuing...")
+	time.Sleep(3 * time.Second)
 
 	return nil
 }
--- main.go
+++ main.go
@@ -1,17 +1,42 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
+	"os"
+	"strings"
 
 	"github.com/trace-panic/linux/initializer"
 )
 
+func ReadInput() string {
+	cfg := initializer.GetConfig()
+	fmt.Printf("%v@%v", cfg.Username, cfg.Hostname)
+
+	reader := bufio.NewReader(os.Stdin)
+	input, _ := reader.ReadString('\n')
+	return strings.TrimSpace(input)
+}
+
+func clearConsole() {
+	fmt.Print("\033[H\033[2J")
+}
+
+func BaseLoop() {
+	clearConsole()
+
+	for {
+		input := ReadInput()
+		fmt.Println(input)
+	}
+}
+
 func main() {
 	if err := initializer.CreateConfig(); err != nil {
 		log.Fatal("Error creating configuration: ", err)
+		return
 	}
 
-	cfg := initializer.GetConfig()
-	fmt.Printf("Configuration: %+v\n", cfg)
+	BaseLoop()
 }
